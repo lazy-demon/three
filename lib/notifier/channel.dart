0102a1e02aaa27f6c878e75c2c24e33d6de721a8
_LazyDemon <jefdayt@gmail.com>
2022-05-20 23:18:27 +0200

    check

--- lib/notifier/channel.dart
+++ lib/notifier/channel.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ChannelNotifier extends StateNotifier<String> {
-  ChannelNotifier() : super("");
+  ChannelNotifier() : super("init");
 
   void setState(String state) {
     this.state = state;
+  }
+
+  String status() {
+    return state;
   }
 }
